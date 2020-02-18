8811f056f82a2201238f105251afe78ca17cba12
Your Name <you@example.com>
2020-02-18 14:08:07 -0500

    chore: migrated to new remote repo

---
+++ js/main.js
@@ -0,0 +1,12 @@
+console.log("keep calm and code on");
+
+    const nameEl = document.getElementById('name');
+
+    document.addEventListener("DOMContentLoaded", function(event) {
+        
+        const subcribeButtonElement = document.getElementById("subscribe")
+        // subscribeButtonElement.addEventListener
+        // onclick
+        // alert()
+
+    })
