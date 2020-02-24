2476a1b9ab066e95d25d6091c99649da8dd6993a
Your Name <you@example.com>
2020-02-24 14:08:16 -0500

    style: applied display:grid to grid container

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-console.log("keep calm and code on");
+console.log("Hello from main.js");
 
     const nameEl = document.getElementById('name');
 
@@ -10,3 +10,18 @@ console.log("keep calm and code on");
         // alert()
 
     })
+
+    var elem = document.querySelector('.main-carousel');
+    var flkty = new Flickity( elem, {
+        // options
+        cellAlign: 'left',
+        contain: true
+    });
+
+    // element argument can be a selector string
+    //   for an individual element
+    var flkty = new Flickity( '.main-carousel', {
+    // options
+    });
+
+    
